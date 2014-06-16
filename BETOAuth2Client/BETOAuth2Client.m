ae0d524d097387184d7f0ba0402b3dd22099bb4c
Seivan Heidari <seivan.heidari@icloud.com>
2014-06-16 15:03:47 +0200

    Use builting base encoding. Fix warnings.

--- BETOAuth2Client/BETOAuth2Client.m
+++ BETOAuth2Client/BETOAuth2Client.m
@@ -5,34 +5,34 @@
 #import "BETURLSessionRequestSerializerJSON.h"
 #import "BETURLSessionResponseSerializerJSON.h"
 
-static NSString * BETBase64EncodedStringFromString(NSString *string) {
-    NSData *data = [NSData dataWithBytes:[string UTF8String] length:[string lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
-    NSUInteger length = [data length];
-    NSMutableData *mutableData = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
-    
-    uint8_t *input = (uint8_t *)[data bytes];
-    uint8_t *output = (uint8_t *)[mutableData mutableBytes];
-    
-    for (NSUInteger i = 0; i < length; i += 3) {
-        NSUInteger value = 0;
-        for (NSUInteger j = i; j < (i + 3); j++) {
-            value <<= 8;
-            if (j < length) {
-                value |= (0xFF & input[j]);
-            }
-        }
-        
-        static uint8_t const kAFBase64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-        
-        NSUInteger idx = (i / 3) * 4;
-        output[idx + 0] = kAFBase64EncodingTable[(value >> 18) & 0x3F];
-        output[idx + 1] = kAFBase64EncodingTable[(value >> 12) & 0x3F];
-        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6)  & 0x3F] : '=';
-        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0)  & 0x3F] : '=';
-    }
-    
-    return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
-}
+//static NSString * BETBase64EncodedStringFromString(NSString *string) {
+//    NSData *data = [NSData dataWithBytes:[string UTF8String] length:[string lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+//    NSUInteger length = [data length];
+//    NSMutableData *mutableData = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+//    
+//    uint8_t *input = (uint8_t *)[data bytes];
+//    uint8_t *output = (uint8_t *)[mutableData mutableBytes];
+//    
+//    for (NSUInteger i = 0; i < length; i += 3) {
+//        NSUInteger value = 0;
+//        for (NSUInteger j = i; j < (i + 3); j++) {
+//            value <<= 8;
+//            if (j < length) {
+//                value |= (0xFF & input[j]);
+//            }
+//        }
+//        
+//        static uint8_t const kAFBase64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+//        
+//        NSUInteger idx = (i / 3) * 4;
+//        output[idx + 0] = kAFBase64EncodingTable[(value >> 18) & 0x3F];
+//        output[idx + 1] = kAFBase64EncodingTable[(value >> 12) & 0x3F];
+//        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6)  & 0x3F] : '=';
+//        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0)  & 0x3F] : '=';
+//    }
+//    
+//    return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
+//}
 
 @interface BETOAuth2ClientManager : NSObject
 @property(nonatomic,strong) NSMutableDictionary * clientMap;
@@ -166,7 +166,7 @@ static NSString * BETBase64EncodedStringFromString(NSString *string) {
                                                       sessionConfiguration:sessionConfiguration
                                                          requestSerializer:request
                                                         responseSerializer:
-                                              [BETURLSessionResponseSerializerJSON serializerWithJSONReadingOptions:NSJSONWritingPrettyPrinted withoutNull:YES] operationQueue:nil];
+                                              [BETURLSessionResponseSerializerJSON serializerWithJSONReadingOptions:kNilOptions withoutNull:YES] operationQueue:nil];
   
   
   [[BETOAuth2ClientManager sharedManager].clientMap setObject:client forKey:theIdentifier];
@@ -188,7 +188,7 @@ static NSString * BETBase64EncodedStringFromString(NSString *string) {
 
 - (void)setAuthorizationHeaderFieldithClientIDAndKey;{
     NSString *basicAuthCredentials = [NSString stringWithFormat:@"%@:%@", self.clientId, self.secretKey];
-    [self.session bet_setValue:[NSString stringWithFormat:@"Basic %@", BETBase64EncodedStringFromString(basicAuthCredentials)] forHTTPHeaderField:@"Authorization"];
+    [self.session bet_setValue:[NSString stringWithFormat:@"Basic %@", [[basicAuthCredentials dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:kNilOptions]] forHTTPHeaderField:@"Authorization"];
 }
 
 
